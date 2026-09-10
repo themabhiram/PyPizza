f1ce4f1dae15896faca4d70d3b1bacbedc03cd63
M ABHIRAM <mabhiram39@gmail.com>
2026-09-10 02:42:31 -0700

    Update pypizza.py

--- pypizza.py
+++ pypizza.py
@@ -9,16 +9,21 @@ if pizza_size == "S":
     bill+=15
 elif pizza_size == "M":
     bill+=20
-else:
+elif pizza_size == "L":
     bill+=25
+else:
+    print("entered invalid option")
 
 if pizza_size == "S":
     pepperoni_small = input("Add pepperoni just 2rs?\nY or N : ")
     if pepperoni_small == "Y":
         bill+=2
-else:
+elif pizza_size == "M" or pizza_size == "L":
     pepperoni = input("Add pepperoni just 3rs?\nY or N : ")
-    bill+=3
+    if pepperoni == "Y":
+        bill+=3
+else:
+    print("entered invalid option.")
 
 chesse = input("Extra chesse just 1rs?\nY or N : ")
 if chesse == "Y":
